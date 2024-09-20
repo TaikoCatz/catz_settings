ecc09be0725d35a30bf2c77522dc3380fd460dad
Apricot <apricot.azumi5@gmail.com>
2024-09-21 05:40:59 +0800

    Fix compatibility with Windows

--- demo.cpp
+++ demo.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <clocale>
 #include <format>
 #include <iostream>
 #include <span>
@@ -7,6 +8,7 @@
 #include <hidapi.h>
 
 constexpr int kMaxStrDescLen = 255;
+constexpr int kReportSize = 64;
 
 void panic(std::wstring_view msg)
 {
@@ -21,14 +23,17 @@ void send_feature(hid_device* handle, std::span<unsigned char> buf)
     if (res < 0) {
         std::wcerr << L"Error: " << hid_error(handle) << L'\n';
     } else {
-        std::wcout << L"Written bytes: " << res << L'\n';
+        std::wcerr << L"Written bytes: " << res << L'\n';
     }
 }
 
 int main(int argc, char* argv[])
 {
+    std::locale loc("");
+    std::wcerr.imbue(loc);
+
     int res;
-    unsigned char buf[65];
+    unsigned char buf[kReportSize];
     wchar_t wstr[kMaxStrDescLen];
 
     res = hid_init();
@@ -38,28 +43,28 @@ int main(int argc, char* argv[])
 
     res = hid_get_manufacturer_string(handle, wstr, kMaxStrDescLen);
     if (res < 0) panic(hid_error(handle));
-    std::wcout << L"Manufacturer String: " << wstr << L'\n';
+    std::wcerr << L"Manufacturer String: " << wstr << L'\n';
 
     res = hid_get_product_string(handle, wstr, kMaxStrDescLen);
     if (res < 0) panic(hid_error(handle));
-    std::wcout << L"Product String: " << wstr << L'\n';
+    std::wcerr << L"Product String: " << wstr << L'\n';
 
     res = hid_get_serial_number_string(handle, wstr, kMaxStrDescLen);
     if (res < 0) panic(hid_error(handle));
-    std::wcout << L"Serial Number String: " << wstr << L'\n';
+    std::wcerr << L"Serial Number String: " << wstr << L'\n';
 
     buf[0] = 0x01;
     res = hid_get_feature_report(handle, buf, 64);
     if (res < 0) panic(hid_error(handle));
 
-    std::wcout << L"Received bytes: " << res << L'\n';
+    std::wcerr << L"Received bytes: " << res << L'\n';
     for (int i = 0; i < res; i++) {
-        std::wcout << std::format(L"{:02x}{}", buf[i], i == res - 1 ? L'\n' : L' ');
+        std::wcerr << std::format(L"{:02x}{}", buf[i], i == res - 1 ? L'\n' : L' ');
     }
 
     if (res == 32) {
         // Write short.
-        send_feature(handle, {buf, 31});
+        send_feature(handle, {buf, kReportSize - 1});
 
         // Write single byte.
         send_feature(handle, {buf, 1});
@@ -67,29 +72,29 @@ int main(int argc, char* argv[])
         auto recover = buf[1];
         // Write corrupted.
         buf[1] = 'x';
-        send_feature(handle, {buf, 32});
+        send_feature(handle, {buf, kReportSize});
 
         // Write wrong report.
         buf[0] = 42;
-        send_feature(handle, {buf, 32});
+        send_feature(handle, {buf, kReportSize});
 
         // Write back.
         buf[0] = 0x01;
         buf[1] = recover;
-        send_feature(handle, {buf, 32});
+        send_feature(handle, {buf, kReportSize});
     }
 
     // Continue get status.
     for (int i = 0; i < 25; ++i) {
-        res = hid_get_feature_report(handle, buf, 32);
+        res = hid_get_feature_report(handle, buf, kReportSize);
         if (res < 0) panic(hid_error(handle));
 
-        std::wcout << L"Received bytes: " << res << L'\n';
+        std::wcerr << L"Received bytes: " << res << L'\n';
         for (int i = 0; i < res; i++) {
-            std::wcout << std::format(L"{:02x}{}", buf[i], i == res - 1 ? L'\n' : L' ');
+            std::wcerr << std::format(L"{:02x}{}", buf[i], i == res - 1 ? L'\n' : L' ');
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
 
     hid_close(handle);
